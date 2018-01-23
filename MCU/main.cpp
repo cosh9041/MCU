10a337790d4501ced277a7403f77d371ed387c7a
Kent Douglas Lee <kele5448@colorado.edu>
2018-01-23 14:06:09 -0700

    Initial commit. Empty atmel project

---
+++ MCU/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * MCU.cpp
+ *
+ * Created: 1/23/2018 2:01:57 PM
+ * Author : kele5448
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
