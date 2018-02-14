144ddb85a157d62bf5708caca481b00ce0465f1c
Pol Sieira <posi7790@colorado.edu>
2018-02-14 12:12:57 -0700

    Add c style headers to header files

--- MCU/lib/calcInducedFric.h
+++ MCU/lib/calcInducedFric.h
@@ -1,18 +1,18 @@
 /*
- * fsInjection.h
+ * calcInducedFric.h
  *
  * Created: 1/29/2018 2:41:34 PM
  * Author : Pol Sieira
  */ 
 
-#ifndef FSINJECTION_H_
-#define FSINJECTION_H_
+#ifndef CALCINDUCEDFRIC_H_
+#define CALCINDUCEDFRIC_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-float fsInjection(int cmdToFaultSensor);
+float calcInducedFric(float omega);
 
 #ifdef __cplusplus
 }
