24f08f9eb9ca23eb78dcf94b945aa3cee1092257
Pol Sieira <posi7790@colorado.edu>
2018-02-14 10:56:56 -0700

    New C files created from old files and standard libs added

---
+++ MCU/lib/fsInjection.c
@@ -0,0 +1,27 @@
+﻿/*
+ * fsInjection.cpp
+ *
+ * Created: 1/29/2018 2:41:34 PM
+ * Author : Pol Sieira
+ */ 
+
+//#include "sam.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "fsInjection.h"
+
+float fsInjection(int cmdToFaultSensor)
+{
+	/*Definition of Variables*/
+	float theta_hat_rel,theta_rel,delta_theta_rel;	
+	
+	if (cmdToFaultSensor == 1)
+	{
+		delta_theta_rel = 10; /*TODO change to better bias*/
+	}
+	else
+	{
+		delta_theta_rel = 0; /*No bias if not commanded to fault*/;
+	}
+return delta_theta_rel;
+}
