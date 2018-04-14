5bafdbcb5b68c7ba88d7ad4fffe37dc75ee5d5bd
cosh9041 <corwin.sheahan@colorado.edu>
2018-04-14 12:07:26 -0600

    Abstract Timed Injection Also add timed injection for fine sensor

--- libraries/fiUtil/fi_util.c
+++ libraries/fiUtil/fi_util.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include <math.h>
 #include "fi_util.h"
 
 void initializeFaultInjectState(FiState *fiState) {
-  fiState->fsBias = 10;
+  fiState->fsBias = 4.0*2.0*M_PI/360;
   fiState->cmdToFaultRW = 0;
   fiState->cmdToFaultFS = 0;
 }
