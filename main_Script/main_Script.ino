5c9497e6db85c0c6095a26a864106c09d344e148
cosh9041 <corwin.sheahan@colorado.edu>
2018-04-14 11:13:16 -0600

    Cleanup
    Works as expected, but did not have chance to run on hardware with
    entire setup. Ran in "dry" mode, aka no air.

--- main_Script/main_Script.ino
+++ main_Script/main_Script.ino
@@ -14,8 +14,14 @@ Servo myservo;
 #include <inttypes.h>
 #include <fsInjection.h>
 
+// Motor defines
 #define PWM_FREQ1  6600
 #define PWM_FREQ2  6600
+#define LOGIC_ANALYZER_PIN 33
+#define PRIMARY_MOTOR_PIN 29
+#define REDUNDANT_MOTOR_PIN 39
+
+// Pixy defines
 #define PRIMARY_FS_PIN 46
 #define SECONDARY_FS_PIN 48
 #define CS_PIN 50
@@ -28,7 +34,7 @@ PixySPI_SS coarsePixy(CS_PIN);
 
 //Define Variables we'll be connecting to
 double deltaThetaRadFine1, deltaThetaRadFine2;
-double deltaThetaRadCoarse1, deltaThetaRadCoarse2;
+double deltaThetaRadCoarse;
 double Setpoint, deltaThetaRad, commandedTorque_mNm;
 
 //Specify the links and initial tuning parameters
@@ -41,7 +47,7 @@ FmState *fmState = &fmBase;
 FiState fiBase;
 FiState *fiState = &fiBase;
 
-unsigned long timeLastReadPixy;
+unsigned long timeLastReadPixy = 0;
 
 void setup() 
 { 
@@ -78,19 +84,17 @@ void setup()
   //Enable Primary Motor Cycle for Initialization; both motor controllers are 
   //written high and switching comes from the pwm signals sent to the motor controller
   //in the loop.
-  digitalWrite(33,HIGH); //set ref of logic analyzer to 3.3V
-  digitalWrite(39,LOW);  //drive redundant motor low (disabled)
-  digitalWrite(29,HIGH); //drive primary motor high (enabled)
+  digitalWrite(LOGIC_ANALYZER_PIN, HIGH); //set ref of logic analyzer to 3.3V
+  digitalWrite(REDUNDANT_MOTOR_PIN, LOW);  //drive redundant motor low (disabled)
+  digitalWrite(PRIMARY_MOTOR_PIN, HIGH); //drive primary motor high (enabled)
   delay(1000);
-  digitalWrite(39,HIGH); //cycle redundant motor high (enabled)
-  digitalWrite(29,LOW); //cycle primary motor low (disabled)
+  digitalWrite(REDUNDANT_MOTOR_PIN, HIGH); //cycle redundant motor high (enabled)
+  digitalWrite(PRIMARY_MOTOR_PIN,LOW); //cycle primary motor low (disabled)
   delay(1000);
-  digitalWrite(29,HIGH);//cycle primary motor high (enabled)
+  digitalWrite(PRIMARY_MOTOR_PIN,HIGH);//cycle primary motor high (enabled)
 
   initializeFaultManagementState(fmState);
   initializeFaultInjectState(fiState);
-  
-  timeLastReadPixy = 0;
 } 
 
 //conversions for torques to PWM bins
@@ -105,8 +109,8 @@ uint32_t pwm_duty_inactive = 127;
 static int i = 0;
 int k;
 char buf[32];
-uint16_t blocks;
-uint16_t fineBlocks;
+uint16_t fineBlocks1;
+uint16_t fineBlocks2;
 uint16_t coarseBlocks;     
 
 // Pixy conversions
@@ -116,13 +120,11 @@ double const centerOffsetDegFine = 160*convertPixToDegFine;
 double const centerOffsetDegCoarse =160*convertPixToDegCoarse;
 double const convertDegToRad = 3.1415926535897932384626433832795/180; 
 
-// Reaction Wheel speed conversions
-
-
 // Reaction wheel speed variable
 uint16_t rwSpeedBin;
-
 float rwSpeedRad; 
+
+// Physical characteristics
 float const p1 = 0.0000335653965; // mNm/(rad/s), viscous friction 
 float const p2 = 0.1303; // mNm, coloumb friction
 float MOI = 1; //MOI of RW, stubbed out for now
@@ -142,19 +144,14 @@ float orderedCommandedTorqueHistory[lengthOfHistory];
 float orderedTimeStampHistory[lengthOfHistory];
 float angularAccel[lengthOfHistory-1];
 
-float mockRWSpeed = 0.0;
-float mockCommandTorque = 0.4;
+// Utility function specifications. Implementations are below loop()
+void sendTorque();
+void getRWSpeed(float *rwSpeedRad, uint16_t analogReading);
+void runFmAndControl();
 
-void getRWSpeed(float *rwSpeedRad, uint16_t analogReading) {
-  // hard coded in the '- 81' portion. This tunes down to 0 rads. most likely our system isn't perfect and is causing this. Not sure tho
-  *rwSpeedRad = (28000/4096*analogReading - 14000)*2*PI/60 - 81;
-}
-
-void loop() 
-{
+void loop() {
   getRWSpeed(&rwSpeedRad, analogRead(A0));
 
-  // Log RW speed.
   storeRWSpeed(reactionWheelSpeedHistory, timeStampHistory, currentIndex, rwSpeedRad, millis());
 
   // Stores ordered lists of last n = `lengthOfHistory` data points, with most recent being at index 0
@@ -164,105 +161,83 @@ void loop()
 
   getAngularAcceleration(orderedRWSpeedHistory, orderedTimeStampHistory, angularAccel, lengthOfHistory);
 
-  
   // NOTE: Pixy.getBlocks does a dirty nasty thing and returns 0 both when there are no blocks
   // detected AND when there is no information. This ambiguous case cost us a lot of wasted time
   if ((millis() - timeLastReadPixy) > 20) {
-    fineBlocks = finePixy2.getBlocks();// NOTE: TO run on board which is not pixy enabled, comment this line out
+    fineBlocks2 = finePixy2.getBlocks();// NOTE: TO run on board which is not pixy enabled, comment this line out
     coarseBlocks = coarsePixy.getBlocks();
     timeLastReadPixy = millis();
   }
 
-  if (fineBlocks) {
-  
+  if (fineBlocks2) {
     // uncomment out these lines to inject a rw fault. DO NOT DELETE UNTIL GSU IS INTEGRATED
-    if (millis() > 40000 && !fiState->cmdToFaultRW) {
-      Serial.println("faulting");
-      fiState->cmdToFaultRW = 1;
-    }
-    if (millis() > 100000 && fiState->cmdToFaultRW) {
-      Serial.println("Unfaulting");
-      fiState->cmdToFaultRW = 0;
-    }
-
+    // if (millis() > 40000 && !fiState->cmdToFaultRW && millis() < 80000) {
+    //   Serial.println("faulting");
+    //   fiState->cmdToFaultRW = 1;
+    // }
+    // if (millis() > 100000 && fiState->cmdToFaultRW) {
+    //   Serial.println("Unfaulting");
+    //   fiState->cmdToFaultRW = 0;
+    // }
     deltaThetaRadFine2 = ((finePixy2.blocks[0].x)*convertPixToDegFine - centerOffsetDegFine)*convertDegToRad;
     fsInjection(&deltaThetaRadFine2, fiState);
     deltaThetaRad = deltaThetaRadFine2;
+
+    runFmAndControl();
     
-    faultManagement(fmState, angularAccel, orderedCommandedTorqueHistory,
-		    lengthOfHistory, MOI);
+  } else if(coarseBlocks) {
+    deltaThetaRadCoarse = ((coarsePixy.blocks[0].x)*convertPixToDegCoarse - centerOffsetDegCoarse)*convertDegToRad;
+    deltaThetaRad = deltaThetaRadCoarse;
+    runFmAndControl();
 
-    // call to Compute assigns output to variable commandedTorque_mNm via pointers
-    myPID.Compute(); 
-
-    // TODO: Test injection strength and tune for delta_omega
-    commandedTorque_mNm = injectRWFault(fiState, commandedTorque_mNm, rwSpeedRad, p1, p2, delta_omega, 
-      fmState->activeRW == 1);
-
-    pwm_duty = (commandedTorque_mNm*mNm_to_mA*mA_to_duty + pwmOffset)*duty_to_bin;
-
-    if (pwm_duty >230) {
-      pwm_duty = 230;
-    }
-    else if (pwm_duty <26) {
-      pwm_duty = 26; 
-    }
-    pwm_duty2 = round(pwm_duty);
-    pwm_duty3 = (uint32_t) pwm_duty2;
-
-    // Testing switching b/w rw
-    //if (millis() < 20000){
-      pwm.pinDuty( 6, pwm_duty3 );  // computed duty cycle on Pin 6 (Primary RW)
-      pwm.pinDuty( 7, pwm_duty_inactive );  // inactive duty cycle (127 PWM) on Pin 7 (Secondary RW)
-    //}
-    //else{
-    //  pwm.pinDuty( 6, pwm_duty_inactive );  // inactive duty cycle (127 PWM) on Pin 6 (Primary RW)
-    //  pwm.pinDuty( 7, pwm_duty3 );  // computed duty cycle on Pin 7 (Secondary RW)
-    //}
-    
-    storeTorqueAndIncrementIndex(commandedTorqueHistory, &currentIndex, commandedTorque_mNm, lengthOfHistory);
-  }else if(coarseBlocks){
-
-    deltaThetaRad = ((coarsePixy.blocks[0].x)*convertPixToDegCoarse - centerOffsetDegCoarse)*convertDegToRad;
-    
-    faultManagement(fmState, angularAccel, orderedCommandedTorqueHistory,
-        lengthOfHistory, MOI);
-
-    // call to Compute assigns output to variable commandedTorque_mNm via pointers
-    myPID.Compute(); 
-
-    // TODO: Test injection strength and tune for delta_omega
-    commandedTorque_mNm = injectRWFault(fiState, commandedTorque_mNm, rwSpeedRad, p1, p2, delta_omega, 
-      fmState->activeRW == 1);
-
-    pwm_duty = (commandedTorque_mNm*mNm_to_mA*mA_to_duty + pwmOffset)*duty_to_bin;
-
-    if (pwm_duty >230) {
-      pwm_duty = 230;
-    }
-    else if (pwm_duty <26) {
-      pwm_duty = 26; 
-    }
-    pwm_duty2 = round(pwm_duty);
-    pwm_duty3 = (uint32_t) pwm_duty2;
-
-    // Testing switching b/w rw
-    //if (millis() < 20000){
-      pwm.pinDuty( 6, pwm_duty3 );  // computed duty cycle on Pin 6 (Primary RW)
-      pwm.pinDuty( 7, pwm_duty_inactive );  // inactive duty cycle (127 PWM) on Pin 7 (Secondary RW)
-    //}
-    //else{
-    //  pwm.pinDuty( 6, pwm_duty_inactive );  // inactive duty cycle (127 PWM) on Pin 6 (Primary RW)
-    // pwm.pinDuty( 7, pwm_duty3 );  // computed duty cycle on Pin 7 (Secondary RW)
-    //}
-
-    storeTorqueAndIncrementIndex(commandedTorqueHistory, &currentIndex, commandedTorque_mNm, lengthOfHistory);
-  }else {
-    // Serial.println("No Blocks detected");
-    // Serial.println(commandedTorque_mNm,5);
+  } else {
     // If we do not pick up blocks set PWM to 50% to shut off motors
     pwm.pinDuty( 6, pwm_duty_inactive );
     pwm.pinDuty( 7, pwm_duty_inactive );
   }
-  //Serial.println(pwm_duty3);
+}
+
+void runFmAndControl() {
+  faultManagement(fmState, angularAccel, orderedCommandedTorqueHistory,
+	    lengthOfHistory, MOI);
+
+  // call to Compute assigns output to variable commandedTorque_mNm via pointers
+  myPID.Compute(); 
+
+  // TODO: Test injection strength and tune for delta_omega
+  commandedTorque_mNm = injectRWFault(fiState, commandedTorque_mNm, rwSpeedRad, p1, p2, delta_omega, 
+    fmState->activeRW == 1);
+
+  sendTorque();
+}
+
+void sendTorque() {
+  pwm_duty = (commandedTorque_mNm*mNm_to_mA*mA_to_duty + pwmOffset)*duty_to_bin;
+
+  if (pwm_duty > 230) {
+    pwm_duty = 230;
+  } else if (pwm_duty < 26) {
+    pwm_duty = 26; 
+  }
+  pwm_duty2 = round(pwm_duty);
+  pwm_duty3 = (uint32_t) pwm_duty2;
+
+  // Testing switching b/w rw
+  if (fmState->activeRW == 1) {
+    pwm.pinDuty( 6, pwm_duty3 );  // computed duty cycle on Pin 6 (Primary RW)
+    pwm.pinDuty( 7, pwm_duty_inactive );  // inactive duty cycle (127 PWM) on Pin 7 (Secondary RW)
+  } else if (fmState->activeRW == 2) {
+    pwm.pinDuty(6, pwm_duty_inactive); // inactive duty cycle (127 PWM) on Pin 6 (Primary RW)
+    pwm.pinDuty(7, pwm_duty3);         // computed duty cycle on Pin 7 (Secondary RW)
+  } else {
+    pwm.pinDuty(6, pwm_duty_inactive); // inactive duty cycle (127 PWM) on Pin 6 (Primary RW)
+    pwm.pinDuty(7, pwm_duty_inactive); // computed duty cycle on Pin 7 (Secondary RW)
+  }
+
+  storeTorqueAndIncrementIndex(commandedTorqueHistory, &currentIndex, commandedTorque_mNm, lengthOfHistory);
+}
+
+void getRWSpeed(float *rwSpeedRad, uint16_t analogReading) {
+  // hard coded in the '- 81' portion. This tunes down to 0 rads. most likely our system isn't perfect and is causing this. Not sure tho
+  *rwSpeedRad = (28000/4096*analogReading - 14000)*2*PI/60 - 81;
 }
